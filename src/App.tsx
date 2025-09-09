1d2fe674bfd92e22d84845d02ecd2dc84567e988
Moa Magnfors <moa@magnfors.se>
2025-09-09 10:41:00 +0200

    💩 style(design system): test change button color

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import "./App.css";
+import { DigiButton } from '@digi/arbetsformedlingen-react';
+import { ButtonVariation } from '@digi/arbetsformedlingen'; // observera att import av enums sker via @digi/arbetsformedlingen
 
-function App() {
-  return <></>;
+export function App() {
+  return (
+    <DigiButton className="my-class" afVariation={ButtonVariation.PRIMARY} onAfOnClick={() => console.log('Hallå världen!')}>
+      Skicka
+    </DigiButton>
+  );
 }
-
 export default App;
