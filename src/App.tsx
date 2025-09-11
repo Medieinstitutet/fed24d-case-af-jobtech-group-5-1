176ca19ef75d3abd1b7f448ad0d4c4423a4ea990
Moa Magnfors <moa@magnfors.se>
2025-09-11 21:56:34 +0200

    ✨ feat(context): add JobContext and JobProvider with useJobs integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import { RouterProvider } from 'react-router';
-import { router } from './Router';
+import { RouterProvider } from "react-router";
+import { router } from "./Router";
+import { JobProvider } from "./context/JobContext";
 
 export function App() {
   return (
-
-
     <>
+      <JobProvider>
         <RouterProvider router={router}></RouterProvider>
+      </JobProvider>
     </>
   );
 }
