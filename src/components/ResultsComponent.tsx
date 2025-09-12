ff918c5fae9ce2f56be2e996c323195415881586
Moa Magnfors <moa@magnfors.se>
2025-09-12 20:43:14 +0200

    ✨ feat: add pagination to ResultsComponent using getPaginationData

--- src/components/ResultsComponent.tsx
+++ src/components/ResultsComponent.tsx
@@ -1,19 +1,57 @@
+import { useState } from "react";
+import { useJobContext } from "../context/JobContext";
 import { ResultCard } from "./ResultCard";
-import "../styles/ResultCard.scss";
 import { DigiNavigationPagination } from "@digi/arbetsformedlingen-react";
+import { useJobs } from "../services/getJobs";
+import { getPaginationData } from "../utils/pagination";
 
 export const ResultsComponent = () => {
+  const { state } = useJobContext();
+  const limit = 25;
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const totalResults = 500; 
+
+    const offset = limit * (currentPage - 1);
+
+          const jobs = useJobs({
+    offset,
+    limit,
+    q: state.query,
+    region: state.region,
+    occupation: state.occupation,
+  });
+
+  const { totalPages, currentResultStart, currentResultEnd } = getPaginationData(
+    currentPage,
+    limit,
+    totalResults,
+    jobs.length
+  );
+
+  const handlePageChange = (event: CustomEvent<number>) => {
+    setCurrentPage(event.detail);
+  };
+
   return (
     <div className="job-list">
-      <ResultCard />
+      <ul>
+        {jobs.length > 0 ? (
+          jobs.map((job) => <ResultCard key={job.id} job={job} />)
+        ) : (
+          <p>Inga jobb hittades</p>
+        )}
+      </ul>
+
       <DigiNavigationPagination
-        afTotalPages={6}
-        afInitActive-page={1}
-        afCurrentResultStart={1}
-        afCurrentResultEnd={25}
-        afTotalResults={10}
+        afTotalPages={totalPages}
+        afInitActivePage={1}
+        afCurrentResultStart={currentResultStart}
+        afCurrentResultEnd={currentResultEnd}
+        afTotalResults={totalResults}
         afResultName="annonser"
         className="pagination"
+        onAfOnPageChange={handlePageChange}
       />
     </div>
   );
