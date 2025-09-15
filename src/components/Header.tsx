167f4ed0682f1ce11598ee08fff4b0b82e51e09d
Sokol Hatija <hatijasokol@gmail.com>
2025-09-15 15:01:17 +0200

    feat(header): changes on header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,56 +1,54 @@
-import { DigiHeader, DigiHeaderNavigation, DigiHeaderNavigationItem, DigiHeaderNotification, DigiIconBellFilled, DigiHeaderAvatar } from "@digi/arbetsformedlingen-react";
-import "../index.css";
+import {
+  DigiHeader,
+  DigiHeaderNavigation,
+  DigiHeaderNavigationItem,
+  DigiHeaderNotification,
+  DigiHeaderAvatar,
+  DigiIconBellFilled,
+} from "@digi/arbetsformedlingen-react";
 import compass from "../assets/compass.png";
+import "../index.css";
 
 export const Header = () => {
-    return (
-        <>
-            <DigiHeader className="header"
+  return (
+  <DigiHeader className="header"
+    afSystemName=""
+    afHideSystemName={true}
+    afMenuButtonText="Meny"
+  >
+      <a slot="header-logo" href="/" className="brand-logo" aria-label="Startsida">
+        <img src={compass} alt="Karriärkompassen" className="brand-img" />
+        <span className="brand-name">Karriärkompassen</span>
+      </a>
 
-                afSystemName=""
-                afHideSystemName={true}
-                afMenuButtonText="Meny"
-            >
-                <div slot="header-logo" aria-label="" className="brand-logo">
-                    <img src={compass} alt="Karriärkompassen" className="brand-img" />
-                    <span className="brand-name">Karriärkompassen</span>
-                </div>
-                <div slot="header-content">
-                    <DigiHeaderNotification afNotificationAmount={8}>
-                        <a href="/">
-                            <DigiIconBellFilled></DigiIconBellFilled>
-                            Notiser
-                        </a>
-                    </DigiHeaderNotification>
-                    <DigiHeaderAvatar
-                        afSrc=""
-                        afAlt=""
-                        afName=""
-                        afSignature="KALIA"
-                        afIsLoggedIn={true}
-                        afHideSignature={true}
-                    ></DigiHeaderAvatar>
-                </div>
-                <div slot="header-navigation">
-                    <DigiHeaderNavigation
-                        afCloseButtonText="Stäng"
-                        afCloseButtonAriaLabel="Stäng meny"
-                        afNavAriaLabel="Huvudmeny"
-                    >
-                        <DigiHeaderNavigationItem afCurrentPage={true}>
-                            <a href="/">Mina bokningar</a>
-                        </DigiHeaderNavigationItem>
-                        <DigiHeaderNavigationItem>
-                            <a href="/">Grupper</a>
-                        </DigiHeaderNavigationItem>
-                        <DigiHeaderNavigationItem>
-                            <a href="/">Kontakt</a>
-                        </DigiHeaderNavigationItem>
-                    </DigiHeaderNavigation>
-                </div>
-            </DigiHeader>
-        </>
-    );
+      <div slot="header-content">
+        <DigiHeaderNotification afNotificationAmount={2}>
+          <a href="/">
+            <DigiIconBellFilled />
+            Notiser
+          </a>
+        </DigiHeaderNotification>
+      </div>
 
+      <div slot="header-navigation">
+        <DigiHeaderNavigation
+          afCloseButtonText="Stäng"
+          afCloseButtonAriaLabel="Stäng meny"
+          afNavAriaLabel="Huvudmeny"
+        >
+          <DigiHeaderNavigationItem afCurrentPage={true}>
+            <a href="/">Mina bokningar</a>
+          </DigiHeaderNavigationItem>
+          <DigiHeaderNavigationItem>
+            <a href="/">Grupper</a>
+          </DigiHeaderNavigationItem>
+          <DigiHeaderNavigationItem>
+            <a href="/">Kontakt</a>
+          </DigiHeaderNavigationItem>
+        </DigiHeaderNavigation>
+      </div>
+    </DigiHeader>
+  );
 };
+
 export default Header;
