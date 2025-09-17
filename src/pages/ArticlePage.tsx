225ff4df640e0d791d302860b091a6eb71efe4a5
Sokol Hatija <hatijasokol@gmail.com>
2025-09-17 11:03:24 +0200

    feat(articlepage): took away uneeded style

--- src/pages/ArticlePage.tsx
+++ src/pages/ArticlePage.tsx
@@ -1,25 +1,25 @@
-import { useParams } from "react-router";
+import { useNavigate, useParams } from "react-router-dom";
 import { articles } from "../data/articles";
-import { DigiLink } from "@digi/arbetsformedlingen-react";
+import { DigiLinkInternal } from "@digi/arbetsformedlingen-react";
 import { LinkVariation } from "@digi/arbetsformedlingen";
 import "../styles/ArticlePage.scss";
 
 export const ArticlePage = () => {
   const { slug } = useParams<{ slug: string }>();
+  const navigate = useNavigate();
   const article = articles.find(a => a.slug === slug);
   if (!article) return <h1>Hittades inte</h1>;
-
 
   return (
     <article className="article-content">
         <div className="back-btn">
-          <DigiLink
+          <DigiLinkInternal
             onAfOnClick={() => {
-              window.history.back();
+              navigate(-1);
             }}
             afVariation={LinkVariation.LARGE}>
             Tillbaka
-          </DigiLink>
+          </DigiLinkInternal>
         </div>
 
       <h1>{article.title}</h1>
