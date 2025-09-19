91dafa8152dc44fe62d2d2111ffa4a10073259cc
Moa Magnfors <moa@magnfors.se>
2025-09-19 12:39:17 +0200

    ✨ feat(router): add SavedJobsPage to router
    Co-Authored-By: Anna Biehl <175021461+Biehlen@users.noreply.github.com>

--- src/Router.tsx
+++ src/Router.tsx
@@ -5,32 +5,37 @@ import { ResultsPage } from "./pages/ResultsPage";
 import { SingleJobPage } from "./pages/SingleJobPage";
 import { ArticlePage } from "./pages/ArticlePage";
 import NotFoundPage from "./pages/NotFoundPage";
+import { SavedJobsPage } from "./pages/SavedJobsPage";
 
 export const router = createHashRouter([
-    {
-        path: "/",
-        element: <Layout />,
-        children: [
-            {
-                index: true,
-                element: <HomePage />,
-            },
-            {
-                path: "result",
-                element: <ResultsPage />,
-            },
-            {
-                path: "result/:id",
-                element: <SingleJobPage />,
-            },
-            {
-                path: "artiklar/:slug",
-                element: <ArticlePage />,
-            },
-            { 
-                path: "*", 
-                element: <NotFoundPage /> 
-            },
-        ],
-    },
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "result",
+        element: <ResultsPage />,
+      },
+      {
+        path: "result/:id",
+        element: <SingleJobPage />,
+      },
+      {
+        path: "artiklar/:slug",
+        element: <ArticlePage />,
+      },
+      {
+        path: "saved",
+        element: <SavedJobsPage />,
+      },
+      {
+        path: "*",
+        element: <NotFoundPage />,
+      },
+    ],
+  },
 ]);
